84f2de174431def9938e7ac47bf4af44f1cfc0f7
rendyariawan <you@example.com>
2024-03-11 10:26:04 +0700

    map

---
+++ resources/views/leaflet/marker.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+
+     <style>
+        #map { height: 500px; }
+     </style>
+@endsection
+
+@section('content')
+ <section class="content">
+      <div class="container-fluid">
+        <div id="map"></div>
+      </div><!-- /.container-fluid -->
+    </section>
+@endsection('content')
+
+@push('javascript')
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin="">
+    </script>
+    <script>
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var circle = L.circle([51.508, -0.11], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map)
+
+        var polygon = L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(map);
+
+        var marker = L.marker([51.5, -0.09]).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().addTo(map);
+
+        
+
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/map', function () {
+    return view('leaflet.home');
+});
+
+Route::get('/marker', function () {
+    return view('leaflet.marker');
+});
+
